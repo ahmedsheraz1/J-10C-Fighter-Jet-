6cdf8be2d397b1e4071c4381deb0e5d4bb7d0759
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 10:12:31 +0500

    Create J-10C Fighter Jet.py

---
+++ J-10C Fighter Jet.py
@@ -0,0 +1,9 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+c = 3-5j
+('The complex number {0} is formed from the real part {0.real} '
+ 'and the imaginary part {0.imag}.').format(c)
+'The complex number (3-5j) is formed from the real part 3.0 and the imaginary part -5.0.'
+coordinates = (3, 5)
+'X: {0[0]};  Y: {0[1]}'.format(coordinates)
+'X: 3;  Y: 5'
