4f0b1ef219cdaf823993b10457859fccc461e607
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:38:52 +0500

    Create J-10C Jets.py

---
+++ J-10C Jets.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'J-10C Jet').hexdigest()
+'cd22fb6a7cabcd33e62b214d34bf0d78eaa264fff0d13af2d690b80fc07f90b181130acdac3171dfb54f50e871f534b01d62d0899f9d2c782113e507937915ee'
